ab0db6a806603be2e116e4df02ce784e25268fa8
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-01-22 19:23:23 +0530

    array practie questions\

--- level_0/5.1 arrays.py
+++ level_0/5.1 arrays.py
@@ -53,7 +53,7 @@ arr.reverse()
 
 # PRACTICE QUESTIONS
 # Find largest element
-n = int(input("enter number of elements: "))
+n = int(input("enter number of elements to find largest element: "))
 arr= []
 for i in range(n):
     num = int(input(f"enter elements{i+1}: "))
@@ -65,7 +65,7 @@ for i in range(1, n):
 print("largest element is ", largest)
     
 # Find smallest element
-n = int(input("enter number of elements: "))
+n = int(input("enter number of elements to find smallest element: "))
 arr= []
 for i in range(n):
     num = int(input(f"enter elements{i+1}: "))
@@ -76,7 +76,7 @@ for i in range(1, n):
         smallest = arr[i]
 print("smallest element is ", smallest)
 # Reverse array
-n = int(input("enter numbers of elements: "))
+n = int(input("enter numbers of elements to reverse array: "))
 arr = []
 for i in range(n):
     num = int(input(f"enter element {i+1}: "))
@@ -87,7 +87,7 @@ for i in range(n-1, -1, -1):
 print("reversed array is ", reversed_arr)
 
 # Find sum of elements
-n = int(input("enter number of elements: " ))
+n = int(input("enter number of elements to find sum of array: " ))
 arr = []
 for i in range(n):
     num = int(input(f"enter element {i+1}: "))
@@ -96,7 +96,7 @@ sum_elements = sum(arr)
 print("sum of elements is ", sum_elements)
 
 # Linear search
-n = int(input("enter number of elements: "))
+n = int(input("enter number of elements for linear search: "))
 arr = []
 for i in range(n):
     num = int(input(f"enter element {i+1}: "))
@@ -112,7 +112,7 @@ if not found:
     print("element not found")
         
 # Count even & odd numbers
-n = int(input("enter number of elements: "))
+n = int(input("enter number of elements to count odd even: "))
 arr =[]
 for i in range(n):
     num = int(input(f"enter elements {i+1}"))
@@ -128,7 +128,7 @@ print("even count is ", even_count)
 print("odd count is ", odd_count)
 
 # Remove duplicates
-n = int(input("enter number of elements: "))
+n = int(input("enter number of elements to remove duplicates: "))
 arr = []
 for i in range(n):
     num = int(input(f"enter numbers {i+1}: "))
@@ -140,7 +140,7 @@ for i in range(n):
 print("array after removing duplicqates is ", unique_arr)
 
 # Second largest element
-n = int(input("enter number of elements: "))
+n = int(input("enter number of elements to find second largest element: "))
 arr = []
 for i in range(n):
     num = int(input(f"enter element {i+1}: "))
@@ -158,5 +158,33 @@ else:
     print("second largest element is ", second)
 
 # Rotate array by 1
+n = int(input("enter number of elements to rotate element: "))
+arr = []
+for i in range(n):
+    arr.append(int(input(f"enter element {i+1}: ")))
+temp = arr[0]
+for i in range(n-1):
+    arr[i] = arr[i+1]
+arr[n-1] = temp
+print("array after left rotation is ", arr)
+
+temp = arr[n-1]
+for i in range(n-1, 0, -1):
+    arr[i] = arr[i-1]
+arr[0] = temp
+print("array after right rotation is ", arr)
 
 # Check if array is sorted
+n = int(input("enter number of elements to check if array is sorted: "))
+arr = []
+for i in range(n):
+    arr.append(int(input("enter elements")))
+sorted = True
+for i in range(n-1):
+    if arr[i]>arr[i+1]:
+        sorted = False
+        break
+if sorted:
+    print("array is sorted")
+else: 
+    print("array is not sorted")
